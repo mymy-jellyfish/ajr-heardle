5b10c8944bf8b9315c1614c17b6b74e20325f360
Glitch (nutritious-hip-balance) <none>
2022-04-29 19:47:48 +0200

    feat(main): add song shuffled full song collection

---
+++ song_collection_shuffler.js
@@ -0,0 +1,568 @@
+const collection = [
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-kaytranada-out-of-1",
+      answer: "The Weeknd, KAYTRANADA - Out of Time (KAYTRANADA Remix)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-swedish-house-mafia",
+      answer: "The Weeknd, Swedish House Mafia - Sacrifice (Remix)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-less-than-zero",
+      answer: "The Weeknd - Less Than Zero",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-take-my-breath-3",
+      answer: "The Weeknd - Take My Breath",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-i-heard-youre",
+      answer: "The Weeknd - I Heard You’re Married (feat. Lil Wayne)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-here-we-go-again",
+      answer: "The Weeknd - Here We Go… Again (feat. Tyler, The Creator)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-dont-break-my-heart",
+      answer: "The Weeknd - Don’t Break My Heart",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-every-angel-is",
+      answer: "The Weeknd - Every Angel is Terrifying",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-dawn-fm",
+      answer: "The Weeknd - Dawn FM",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-best-friends",
+      answer: "The Weeknd - Best Friends",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-is-there-someone",
+      answer: "The Weeknd - Is There Someone Else?",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-a-tale-by-quincy",
+      answer: "The Weeknd - A Tale By Quincy",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-how-do-i-make-you",
+      answer: "The Weeknd - How Do I Make You Love Me?",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-phantom-regret-by",
+      answer: "The Weeknd - Phantom Regret by Jim",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-gasoline",
+      answer: "The Weeknd - Gasoline",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-starry-eyes",
+      answer: "The Weeknd - Starry Eyes",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-sacrifice",
+      answer: "The Weeknd - Sacrifice",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-out-of-time",
+      answer: "The Weeknd - Out of Time",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-the-morning",
+      answer: "The Weeknd - The Morning (Original)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-the-party-the-after",
+      answer: "The Weeknd - The Party & The After Party (Original)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-high-for-this",
+      answer: "The Weeknd - High For This (Original)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-wicked-games",
+      answer: "The Weeknd - Wicked Games (Original)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-what-you-need",
+      answer: "The Weeknd - What You Need (Original)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-loft-music-original",
+      answer: "The Weeknd - Loft Music (Original)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-the-knowing",
+      answer: "The Weeknd - The Knowing (Original)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-house-of-balloons",
+      answer:
+        "The Weeknd - House Of Balloons / Glass Table Girls (Original)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-coming-down",
+      answer: "The Weeknd - Coming Down (Original)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/nothing-compares-bonus-track",
+      answer: "The Weeknd - Nothing Compares (Bonus Track)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/missed-you-bonus-track",
+      answer: "The Weeknd - Missed You (Bonus Track)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/final-lullaby-bonus-track",
+      answer: "The Weeknd - Final Lullaby (Bonus Track)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/after-hours-the-blaze-remix",
+      answer: "The Weeknd - After Hours (The Blaze Remix)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/scared-to-live-snl-live",
+      answer: "The Weeknd - Scared To Live (SNL Live)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/save-your-tears-opn-remix",
+      answer: "The Weeknd - Save Your Tears (OPN Remix)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/heartless-vapor-wave-remix",
+      answer:
+        "The Weeknd - Heartless (Vapor Wave Remix) [feat. Lil Uzi Vert]",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/blinding-lights-chromatics",
+      answer: "The Weeknd - Blinding Lights (Chromatics Remix)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/blinding-lights-major-lazer",
+      answer: "The Weeknd - Blinding Lights (Major Lazer Remix)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/faith",
+      answer: "The Weeknd - Faith",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/hardest-to-love",
+      answer: "The Weeknd - Hardest To Love",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/escape-from-la",
+      answer: "The Weeknd - Escape From LA",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/snowchild",
+      answer: "The Weeknd - Snowchild",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/alone-again",
+      answer: "The Weeknd - Alone Again",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/until-i-bleed-out",
+      answer: "The Weeknd - Until I Bleed Out",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/scared-to-live",
+      answer: "The Weeknd - Scared To Live",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/too-late",
+      answer: "The Weeknd - Too Late",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/repeat-after-me-interlude",
+      answer: "The Weeknd - Repeat After Me (Interlude)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/in-your-eyes",
+      answer: "The Weeknd - In Your Eyes",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/save-your-tears",
+      answer: "The Weeknd - Save Your Tears",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/after-hours",
+      answer: "The Weeknd - After Hours",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/blinding-lights",
+      answer: "The Weeknd - Blinding Lights",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/heartless",
+      answer: "The Weeknd - Heartless",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/power-is-power",
+      answer: "SZA & The Weeknd & Travis Scott - Power is Power",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/call-out-my-name-a-cappella",
+      answer: "The Weeknd - Call Out My Name (A Cappella)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/try-me",
+      answer: "The Weeknd - Try Me",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/hurt-you-feat-gesaffelstein",
+      answer: "The Weeknd, Gesaffelstein - Hurt You",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/i-was-never-there-feat",
+      answer: "The Weeknd, Gesaffelstein - I Was Never There",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/call-out-my-name",
+      answer: "The Weeknd - Call Out My Name",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/wasted-times",
+      answer: "The Weeknd - Wasted Times",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/privilege",
+      answer: "The Weeknd - Privilege",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/pray-for-me-1",
+      answer: "The Weeknd, Kendrick Lamar - Pray For Me",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/secrets-1",
+      answer: "The Weeknd - Secrets",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/all-i-know-1",
+      answer: "The Weeknd - All I Know (feat. Future)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/reminder-1",
+      answer: "The Weeknd - Reminder",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/rockin-1",
+      answer: "The Weeknd - Rockin’",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/die-for-you-1",
+      answer: "The Weeknd - Die For You",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/love-to-lay-1",
+      answer: "The Weeknd - Love To Lay",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/six-feet-under-1",
+      answer: "The Weeknd - Six Feet Under",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/nothing-without-you-1",
+      answer: "The Weeknd - Nothing Without You",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/a-lonely-night-1",
+      answer: "The Weeknd - A Lonely Night",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/sidewalks-1",
+      answer: "The Weeknd - Sidewalks (feat. Kendrick Lamar)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/attention-1",
+      answer: "The Weeknd - Attention",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/true-colors-1",
+      answer: "The Weeknd - True Colors",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/ordinary-life-1",
+      answer: "The Weeknd - Ordinary Life",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/stargirl-interlude-1",
+      answer: "The Weeknd - Stargirl Interlude (feat. Lana Del Rey)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/party-monster-1",
+      answer: "The Weeknd - Party Monster",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/i-feel-it-coming-1",
+      answer: "The Weeknd - I Feel It Coming (feat. Daft Punk)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/false-alarm",
+      answer: "The Weeknd - False Alarm",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/starboy-1",
+      answer: "The Weeknd - Starboy (feat. Daft Punk)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/pass-dat-the-weeknd-remix",
+      answer: "Pass Dat (The Weeknd Remix) - Jeremih + The Weeknd",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/cant-feel-my-face-martin",
+      answer: "The Weeknd - Can't Feel My Face (Martin Garrix Remix)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-hills-rl-grime-remix",
+      answer: "The Weeknd - The Hills (RL Grime Remix)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/earned-it-fifty-shades-of-4",
+      answer: "The Weeknd - Earned It (Fifty Shades Of Grey)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/acquainted",
+      answer: "The Weeknd - Acquainted",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/angel",
+      answer: "The Weeknd - Angel",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/shameless-1",
+      answer: "The Weeknd - Shameless",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/tell-your-friends",
+      answer: "The Weeknd - Tell Your Friends",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/real-life-1",
+      answer: "The Weeknd - Real Life",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/as-you-are",
+      answer: "The Weeknd - As You Are",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/prisoner",
+      answer: "The Weeknd - Prisoner (feat. Lana Del Rey)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/losers",
+      answer: "The Weeknd - Losers (feat. Labrinth)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/often-1",
+      answer: "The Weeknd - Often",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/in-the-night-1",
+      answer: "The Weeknd - In The Night",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-hills-1",
+      answer: "The Weeknd - The Hills",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/dark-times",
+      answer: "The Weeknd - Dark Times (feat. Ed Sheeran)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/where-you-belong-sohn-remix-1",
+      answer:
+        "The Weeknd - Where You Belong (SOHN Remix (From Fifty Shades Of Grey Remixed))",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/earned-it-fifty-shades-of-1",
+      answer:
+        "The Weeknd - Earned It (Fifty Shades Of Grey) (Marian Hill Remix (From Fifty Shades Of Grey Remixed))",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/often-kygo-remix",
+      answer: "The Weeknd - Often (Kygo Remix)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/cant-feel-my-face-1",
+      answer: "The Weeknd - Can't Feel My Face",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/where-you-belong-from-the-1",
+      answer: `The Weeknd - Where You Belong (From "Fifty Shades Of Grey" Soundtrack)`,
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-king-of-the-fall",
+      answer: "The Weeknd - King Of The Fall",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/wanderlust-steve-pitron-max-4",
+      answer:
+        "The Weeknd - Wanderlust (Steve Pitron & Max Sanna Radio Edit)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/wanderlust-clean-bandit-1-1",
+      answer: "The Weeknd - Wanderlust (Clean Bandit Remix)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/or-nah-remix-feat-the-weeknd",
+      answer: "Or Nah (Remix Feat. The Weeknd)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/drunk-in-love-the-weeknd-remix",
+      answer: "Drunk In Love (The Weeknd Remix)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/kiss-land-2",
+      answer: "The Weeknd - Kiss Land",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/pretty-1",
+      answer: "The Weeknd - Pretty",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/professional-1",
+      answer: "The Weeknd - Professional",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/tears-in-the-rain-1",
+      answer: "The Weeknd - Tears In The Rain",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/love-in-the-sky-3",
+      answer: "The Weeknd - Love In The Sky",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/belong-to-the-world-1",
+      answer: "The Weeknd - Belong To The World",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/adaptation-1",
+      answer: "The Weeknd - Adaptation",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/live-for-1",
+      answer: "The Weeknd - Live For (feat. Drake)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/devil-may-cry-from-the-hunger",
+      answer:
+        "The Weeknd - Devil May Cry (From “The Hunger Games: Catching Fire” Soundtrack)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/wanderlust-2",
+      answer: "The Weeknd - Wanderlust (Pharrell Remix)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-town-1",
+      answer: "The Weeknd - The Town",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/wanderlust-1",
+      answer: "The Weeknd - Wanderlust",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/twenty-eight-album-version-1",
+      answer: "The Weeknd - Twenty Eight",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/till-dawn-here-comes-the-sun",
+      answer: "The Weeknd - Till Dawn (Here Comes The Sun)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/valerie-album-version",
+      answer: "The Weeknd - Valerie",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-initiation-original",
+      answer: "The Weeknd - Initiation (Original)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-same-old-song",
+      answer: "The Weeknd - Same Old Song (Original) [feat. Juicy J]",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-montreal-original",
+      answer: "The Weeknd - Montreal (Original)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-xo-the-host",
+      answer: "The Weeknd - XO / The Host (Original)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-d-d-original",
+      answer: "The Weeknd - D.D. (Original)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-the-fall-original",
+      answer: "The Weeknd - The Fall (Original)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-echoes-of-silence",
+      answer: "The Weeknd - Echoes Of Silence (Original)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-outside-original",
+      answer: "The Weeknd - Outside (Original)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-next-original",
+      answer: "The Weeknd - Next (Original)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-the-birds-pt-2",
+      answer: "The Weeknd - The Birds Pt. 2 (Original)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-rolling-stone",
+      answer: "The Weeknd - Rolling Stone (Original)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-thursday-original",
+      answer: "The Weeknd - Thursday (Original)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-life-of-the-party",
+      answer: "The Weeknd - Life Of The Party (Original)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-heaven-or-las-vegas",
+      answer: "The Weeknd - Heaven Or Las Vegas (Original)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-lonely-star",
+      answer: "The Weeknd - Lonely Star (Original)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-the-birds-pt-1",
+      answer: "The Weeknd - The Birds Pt. 1 (Original)",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-the-zone-original",
+      answer: "The Weeknd - The Zone (Original) [feat. Drake]",
+    },
+    {
+      url: "https://soundcloud.com/theweeknd/the-weeknd-gone-original",
+      answer: "The Weeknd - Gone (Original)",
+    },
+  ]
+
+
+
+const shuffleArray = array => {
+for (let i = array.length - 1; i > 0; i--) {
+const j = Math.floor(Math.random() * (i + 1));
+const temp = array[i];
+array[i] = array[j];
+array[j] = temp;
+}
+}
+
+shuffleArray(collection)
+console.log(collection)
+const collectionTitles = collection.map(track => track.answer);
+collectionTitles.forEach(title => console.log(`"${title}",`));
